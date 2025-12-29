e12a8101cd41fe429f3c132b1d633ae58e03243f
Arvind Kumar <arvindkumar@192.168.1.6>
2025-12-29 23:53:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import QuoteApp from "./components/QuoteApp"
+import './components/QuoteApp.css'
+const App = () => {
+  return (
+    <div>
+      <QuoteApp />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/QuoteApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuoteApp = () => {
+  return (
+    <div>
+      Quote App
+    </div>
+  )
+}
+
+export default QuoteApp
